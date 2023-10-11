b8be092e7ecfe53ddbff82f77118ceede0cf2da8
Ian Yap <ianyapzy@gmail.com>
2023-10-11 15:22:56 +1300

    Basic layout of main menu and start button

--- app/src/main/java/com/ianyap/mobiusinfinite/MainMenuActivity.java
+++ app/src/main/java/com/ianyap/mobiusinfinite/MainMenuActivity.java
@@ -2,13 +2,22 @@ package com.ianyap.mobiusinfinite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.ianyap.mobiusinfinite.databinding.ActivityMainMenuBinding;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        ActivityMainMenuBinding activityMainMenu = ActivityMainMenuBinding.inflate(getLayoutInflater());
+        setContentView(activityMainMenu.getRoot());
+
+        Button startButton = activityMainMenu.startButton;
+        Intent startGame = new Intent(this, GameActivity.class);
+        startButton.setOnClickListener(view -> this.startActivity(startGame));
     }
 }
